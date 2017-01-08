cbc2bc68ed338fb3874d2954a21a8d55c023df49
Alex Buyanov <alex.bu.89@gmail.com>
2017-01-08 19:39:25 +0100

    use jpegoptim to compress img

--- jpeg.sh
+++ jpeg.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
-shopt -s nocaseglob # case insensitive
+
+# case insensitive
+shopt -s nocaseglob
+
+# set directory folder
 IMG_PATH=$1
+OUT_PATH="compressed"
 
 if [ -z "$IMG_PATH" ]; then
   IMG_PATH=$PWD
 fi
 
+# start compression
 echo "Starting to compress $IMG_PATH folder..."
+mkdir $IMG_PATH/$OUT_PATH
 
-for img in "$IMG_PATH"/*.JPG
+for img in "$IMG_PATH"/*.jpg
 do
-  jpegtran -optimize -progressive -outfile $img
+  #jpegtran -optimize -progressive -outfile $img $img
+  jpegoptim -o -m80 -d $IMG_PATH/$OUT_PATH $img
 done
